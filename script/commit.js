2132c1f38e820952bfa20a895690dba92697adaa
Pakin Puttanukulchai <65996113+OnFireByte@users.noreply.github.com>
2023-08-15 15:58:26 +0700

    feat: auto commit

--- script/commit.js
+++ script/commit.js
@@ -1,6 +1,37 @@
-// console.log from command param
+function buildMDString(data) {
+    const { title, tags, recommend_by_user, recommend_from_server, package_url } = data;
+    const tagsString = tags.map((tag) => `- ${tag}`).join("\n");
 
-const commit = process.argv[2];
+    return `---\n\r
+    title: ${title} \n\r
+    tag: \n\r
+    ${tagsString} \n\r
+    by: ${recommend_by_user} \n\r
+    from: ${recommend_from_server} \n\r
+    ---\n\r
+    \n\r
+    ${package_url} \n\r
+    `;
+}
 
-console.log("TEST");
-console.log(commit);
+const raw = process.argv[2];
+
+const data = JSON.parse(raw);
+
+const stored = fs.readFileSync("./data.json", "utf-8");
+
+const storedData = JSON.parse(stored);
+
+// add new package only if it's not exist
+if (data?.package_url) {
+    storedData.push(data);
+
+    fs.writeFileSync("./data.json", JSON.stringify(storedData));
+}
+
+// build all md files every times
+for (const item of storedData) {
+    const mdString = buildMDString(item);
+
+    fs.writeFileSync(`./src/pages/thins/${item.title}.md`, mdString);
+}
