36f366d8007b9ddf2c9c7c98df4f02acbf234f39
Pakin Puttanukulchai <65996113+OnFireByte@users.noreply.github.com>
2023-08-15 22:58:21 +0700

    feat: build README

--- script/commit.js
+++ script/commit.js
@@ -16,9 +16,30 @@ ${package_url} \n
 `;
 }
 
+async function getDesc(url) {
+    const res = await fetch(url);
+    const html = await res.text();
+    const descriptionMatch = html.match(/<meta name="description" content="([^"]+?)"(.?)>/);
+
+    const description = descriptionMatch ? descriptionMatch[1] : "";
+    return description;
+}
+
+function cmp(f) {
+    return (a, b) => {
+        if (f(a) < f(b)) {
+            return -1;
+        }
+        if (f(a) > f(b)) {
+            return 1;
+        }
+        return 0;
+    };
+}
+
 const raw = process.env.payload_data;
 
-const data = JSON.parse(raw);
+const data = raw ? JSON.parse(raw) : null;
 
 console.log("data", data);
 
@@ -33,9 +54,42 @@ if (data?.package_url) {
     fs.writeFileSync("./data.json", JSON.stringify(storedData));
 }
 
+storedData.sort(cmp((v) => v.package_name.toLowerCase()));
+
 // build all md files every times
 for (const item of storedData) {
     const mdString = buildMDString(item);
 
     fs.writeFileSync(`./src/pages/things/${item.package_name.replace("/", "-")}.md`, mdString);
 }
+
+const tagMap = new Map();
+
+for (const item of storedData) {
+    for (const tag of item.tags) {
+        if (tagMap.has(tag)) {
+            const current = tagMap.get(tag);
+            current.push(item);
+        } else {
+            tagMap.set(tag, [item]);
+        }
+    }
+}
+
+let mdString = fs.readFileSync("./script/template/readme-header.md", "utf-8");
+
+const tagGroup = Array.from(tagMap).sort(cmp((v) => v[0].toLowerCase()));
+console.log("tagGroup", tagGroup);
+
+for (let [tag, items] of tagGroup) {
+    tag = tag[0].toUpperCase() + tag.slice(1);
+    mdString += `## ${tag}\n\n`;
+
+    for (const item of items) {
+        const desc = await getDesc(item.package_url);
+
+        mdString += `- [${item.package_name}](${item.package_url})\n`;
+    }
+}
+
+fs.writeFileSync("./README.md", mdString);
